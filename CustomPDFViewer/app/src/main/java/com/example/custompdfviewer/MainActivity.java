54b30bf351d12b227e0e17cf038a65e1ac5abf85
ShAoN <27mdshaonali@gmail.com>
2024-10-14 02:04:24 +0600

    2nd commit and 2nd update.

--- CustomPDFViewer/app/src/main/java/com/example/custompdfviewer/MainActivity.java
+++ CustomPDFViewer/app/src/main/java/com/example/custompdfviewer/MainActivity.java
@@ -8,17 +8,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        pdfView = findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("ethical_hacking.pdf")
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .spacing(0)
+                .load();
+
+
     }
 }
